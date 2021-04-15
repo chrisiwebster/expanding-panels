63c6be695ffa8440a18ea04364b105ee85088edf
Chrisi Webster <christinawebster.cw@gmail.com>
2021-04-15 10:08:32 +0100

    first commit

---
+++ expandingCards/script.js
@@ -0,0 +1,15 @@
+const panels = document.querySelectorAll(".panel");
+
+//looping through each panel
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
